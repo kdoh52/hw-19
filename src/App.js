e309c771a3716680eb4ddf9663c3dcdc75f6a617
kdoh52 <kdoh52@gmail.com>
2020-11-13 19:22:31 -0800

    clean up

--- src/App.js
+++ src/App.js
@@ -5,37 +5,19 @@ import Title from './components/Title';
 import employees from './employees.json';
 import SortButtons from './components/SortButtons';
 
-
-
 let testSort = employees
-
-// by id
-let newArray = testSort.sort((a, b) => (a.id > b.id) ? 1 : -1)
-
-// by name
-// let newArray = testSort.sort((a, b) => (a.name > b.name) ? 1 : -1)
-
-// by birthday
-// let newArray = testSort.sort((a, b) => (a.dob > b.dob) ? 1 : -1)
-
-console.log(newArray);
-
 
 class App extends React.Component {
   state = {
     employees: employees
   };
 
-  // removeFriend = (id) => {
-  //   const newArray = this.state.employees.filter(item => item.id !== id);
-  //   this.setState({employees: newArray})
-  // }
   sortId = event => {
     event.preventDefault();
-    console.log("sortId reached")
+    // console.log("sortId reached")
 
     let newArray = testSort.sort((a, b) => (a.id > b.id) ? 1 : -1)
-    console.log(newArray)
+    // console.log(newArray)
     this.setState({
       employees: newArray
     });
@@ -43,10 +25,10 @@ class App extends React.Component {
 
   sortName = event => {
     event.preventDefault();
-    console.log("sortName reached")
+    // console.log("sortName reached")
 
     let newArray = testSort.sort((a, b) => (a.name > b.name) ? 1 : -1)
-    console.log(newArray)
+    // console.log(newArray)
     this.setState({
       employees: newArray
     });
@@ -54,10 +36,10 @@ class App extends React.Component {
 
   sortDob = event => {
     event.preventDefault();
-    console.log("sortDob reached")
+    // console.log("sortDob reached")
 
     let newArray = testSort.sort((a, b) => (a.dob > b.dob) ? 1 : -1)
-    console.log(newArray)
+    // console.log(newArray)
     this.setState({
       employees: newArray
     });
