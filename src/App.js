d1f283bd0071ec4f3430a9b9487e7a5a77f51037
kdoh52 <kdoh52@gmail.com>
2020-11-13 17:07:02 -0800

    found ways to sort array

--- src/App.js
+++ src/App.js
@@ -4,15 +4,32 @@ import Wrapper from './components/Wrapper';
 import Title from './components/Title';
 import employees from './employees.json';
 
+
+
+let testSort = employees
+
+// by id
+let newArray = testSort.sort((a, b) => (a.id > b.id) ? 1 : -1)
+
+// by name
+// let newArray = testSort.sort((a, b) => (a.name > b.name) ? 1 : -1)
+
+// by birthday
+// let newArray = testSort.sort((a, b) => (a.dob > b.dob) ? 1 : -1)
+
+console.log(newArray);
+
+
 class App extends React.Component {
   state = {
     employees: employees
   };
 
-  removeFriend = (id) => {
-    const newArray = this.state.employees.filter(item => item.id !== id);
-    this.setState({employees: newArray})
-  }
+  // removeFriend = (id) => {
+  //   const newArray = this.state.employees.filter(item => item.id !== id);
+  //   this.setState({employees: newArray})
+  // }
+  
 
   render() {
     return (
